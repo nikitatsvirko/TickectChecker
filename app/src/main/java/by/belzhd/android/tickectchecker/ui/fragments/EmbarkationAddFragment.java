f86a6d91b64784d45147fdab4ebf4cecf1512b77
Nikita Tsvirko <n.tsvirko@softteco.com>
2018-05-17 01:55:04 +0300

    updated ui

--- app/src/main/java/by/belzhd/android/tickectchecker/ui/fragments/EmbarkationAddFragment.java
+++ app/src/main/java/by/belzhd/android/tickectchecker/ui/fragments/EmbarkationAddFragment.java
@@ -1,10 +1,14 @@
 package by.belzhd.android.tickectchecker.ui.fragments;
 
 import android.view.View;
+import android.widget.Button;
 
 import by.belzhd.android.tickectchecker.R;
 
-public class EmbarkationAddFragment extends AbstractFragment {
+public class EmbarkationAddFragment extends AbstractFragment implements View.OnClickListener {
+
+    private Button addEmbButton;
+    private Button cancelEmbButton;
 
     public static EmbarkationAddFragment newInstance() {
         return new EmbarkationAddFragment();
@@ -12,7 +16,11 @@ public class EmbarkationAddFragment extends AbstractFragment {
 
     @Override
     protected void initUi(View view) {
+        addEmbButton = view.findViewById(R.id.addEmbButton);
+        cancelEmbButton = view.findViewById(R.id.cancelEmbButton);
 
+        addEmbButton.setOnClickListener(this);
+        cancelEmbButton.setOnClickListener(this);
     }
 
     @Override
@@ -23,5 +31,26 @@ public class EmbarkationAddFragment extends AbstractFragment {
     @Override
     int getTitleResId() {
         return  R.string.title_embarkation;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addEmbButton:
+                addPerson();
+                break;
+            case R.id.cancelEmbButton:
+                cancelAction();
+                break;
+        }
+    }
+
+    private void cancelAction() {
+        getActivity().onBackPressed();
+    }
+
+    private void addPerson() {
+        showToast("Пассажир добавлен");
+        getActivity().onBackPressed();
     }
 }
