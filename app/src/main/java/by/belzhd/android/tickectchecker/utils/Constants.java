c93ca070b67db3449dc940a279b8cbefc4b1fac1
Nikita Tsvirko <n.tsvirko@softteco.com>
2018-05-19 01:21:30 +0300

    changed names for db files

--- app/src/main/java/by/belzhd/android/tickectchecker/utils/Constants.java
+++ app/src/main/java/by/belzhd/android/tickectchecker/utils/Constants.java
@@ -1,6 +1,6 @@
 package by.belzhd.android.tickectchecker.utils;
 
 public class Constants {
-    public static String GENERAL_DB = "general";
-    public static String REPORTS_DB = "report";
+    public static String GENERAL_DB = "general.db";
+    public static String REPORTS_DB = "report.db";
 }
