75530aad94af582f4850c9ee7c659100f72fa219
Nikita Tsvirko <n.tsvirko@softteco.com>
2018-05-22 22:04:42 +0300

    qr code scanner added

--- app/src/main/java/by/belzhd/android/tickectchecker/ui/activity/MainActivity.java
+++ app/src/main/java/by/belzhd/android/tickectchecker/ui/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package by.belzhd.android.tickectchecker.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,6 +13,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import by.belzhd.android.tickectchecker.R;
 import by.belzhd.android.tickectchecker.ui.fragments.DisembarkationFragment;
@@ -41,6 +46,22 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         fragmentManager = getSupportFragmentManager();
         navigation.setSelectedItemId(R.id.navigation_embarkation);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, R.string.cancelled_text, Toast.LENGTH_LONG).show();
+            } else {
+                if (fragment instanceof EmbarkationFragment) {
+                    EmbarkationFragment.onCodeScaned(result.getContents());
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
